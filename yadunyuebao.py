24ee51e82e20495b4391001bc37f89943a595c61
Ausar <Ausar109@aliyun.com>
2019-02-18 15:07:34 +0800

    0218

--- yadunyuebao.py
+++ yadunyuebao.py
@@ -1,18 +1,17 @@
 from ADB import aalc, set_pause, swtime, timedelta
 from ADB.actionfunc import datetime
 
-set_pause(0.2, 0.4)
+dt = datetime.now() - timedelta(20)
+
+year, month = dt.year, dt.month
+yuefen = month
+
+t1 = '%s-%s-1' % (year, month)
+t2 = '%s-%s-1' % (year, month + 1)
+t1, t2 = swtime(t1), swtime(swtime(t2, False) - timedelta(1))
 
 
 def run(with_goumai=False):
-    #yuefen = datetime.now().month - 1
-    yuefen = 12
-
-    t1 = '2018-' + str(yuefen) + '-1'
-    t2 = '2018-' + str(yuefen + 1) + '-1'
-
-    t2 = '2019-1-1'
-    t1, t2 = swtime(t1), swtime(swtime(t2, False) - timedelta(1))
 
     names = '购买' if with_goumai else ''
 
@@ -21,7 +20,23 @@ def run(with_goumai=False):
         if with_goumai:
             aalc.dp(5, t1, t2)
 
-    
+    cp2()
+    aalc.wt(1, t1, t2, 1)
+    aalc.cnxh(1, t1, t2, 2)
+    aalc.shyjs(1, t1, t2, 2)
+    aalc.bmqd(1, t1, t2, 2)
+    aalc.yhh(1, t1, t2, 2)
+    aalc.sp(str(yuefen) + '月份全部内容' + names)
+
+    cp2()
+    aalc.wt(1, t1, t2, 1)
+    aalc.wt(1, t1, t2, 3)
+    aalc.cnxh(1, t1, t2, 2)
+    aalc.shyjs(1, t1, t2, 2)
+    aalc.bmqd(1, t1, t2, 2)
+    aalc.yhh(1, t1, t2, 2)
+    aalc.sp(str(yuefen) + '月份x微淘' + names)
+
     cp2()
     aalc.cnxh(1, t1, t2, 1)
     aalc.wt(1, t1, t2, 3)
@@ -31,7 +46,43 @@ def run(with_goumai=False):
     aalc.yhh(1, t1, t2, 2)
     aalc.sp(str(yuefen) + '月份x猜你喜欢' + names)
 
-    
+    cp2()
+    aalc.shyjs(1, t1, t2, 1)
+    aalc.wt(1, t1, t2, 3)
+    aalc.cnxh(1, t1, t2, 2)
+    aalc.shyjs(1, t1, t2, 2)
+    aalc.bmqd(1, t1, t2, 2)
+    aalc.yhh(1, t1, t2, 2)
+    aalc.sp(str(yuefen) + '月份x生活研究所' + names)
 
-#run(with_goumai=False)
+    cp2()
+    aalc.bmqd(1, t1, t2, 1)
+    aalc.wt(1, t1, t2, 3)
+    aalc.cnxh(1, t1, t2, 2)
+    aalc.shyjs(1, t1, t2, 2)
+    aalc.bmqd(1, t1, t2, 2)
+    aalc.yhh(1, t1, t2, 2)
+    aalc.sp(str(yuefen) + '月份x必买清单' + names)
+
+    cp2()
+    aalc.yhh(1, t1, t2, 1)
+    aalc.wt(1, t1, t2, 3)
+    aalc.cnxh(1, t1, t2, 2)
+    aalc.shyjs(1, t1, t2, 2)
+    aalc.bmqd(1, t1, t2, 2)
+    aalc.yhh(1, t1, t2, 2)
+    aalc.sp(str(yuefen) + '月份x有好货' + names)
+
+aalc.cp()
+aalc.tm(3, t1, t2)
+aalc.sp(str(yuefen) + '月份-粉丝')
+
+
+aalc.cp()
+aalc.dp(5, t1, t2)
+aalc.tm(3, t1, t2)
+aalc.sp(str(yuefen) + '月份-粉丝购买')
+
+
+run(with_goumai=False)
 run(with_goumai=True)
