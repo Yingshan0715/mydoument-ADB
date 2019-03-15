1d8e22c502443fdf68e6ede9941acefd5ecdf2ed
Ausar109 <Ausar109@aliyun.com>
2019-03-15 17:39:54 +0800

    0315

--- aaa.py
+++ aaa.py
@@ -5,27 +5,29 @@ from datetime import timedelta
 from ADB import shan_renqun
 
 
-set_dglc(2, False, 'dp', 0, 0, 2, 2, 2, '雅培')
+set_dglc(2, False, 'dp', 0, 0, 2, 2, 2, '维多利亚的秘密')
 
 tAs = t180
 tIs = t180
-tHs = somedays('2019-3-7', 90)
-tpend = '2019-3-9'
+tHs = somedays('2019-3-3', 90)
+tpend = '2019-3-2'
+
+sousuoci = '维多利亚的秘密,维秘'
+
+fuzhu_of_depth(tAs, tIs, tHs, tpend, sousuoci, pph=False)
+input('...')
+depths_of_aipl(tAs, tIs, tpend)
 
 
-#fuzhu_of_depth(tAs, tIs, tHs, tpend, sousuoci, pph=False)
-#depths_of_aipl(tAs, tIs, tpend)
-
-
-t2bef = '2019-3-10'
-t2end = '2019-3-13'
-
+t2bef = '2019-3-3'
+t2end = '2019-3-9'
+input('...')
 #diaoyong_jindian(t2bef, t2end)
 #diaoyong_tracking(t2bef, t2end, tpend, 'bg')
 #diaoyong_tracking(t2bef, t2end, tpend, 'scjg')
 #diaoyong_tracking(t2bef, t2end, tpend, 'jg')
-#diaoyong_tracking(t2bef, t2end, tpend, 'gm')
-diaoyong_tracking(t2bef, t2end, tpend, 'jd')
+diaoyong_tracking(t2bef, t2end, tpend, 'gm')
+#diaoyong_tracking(t2bef, t2end, tpend, 'jd')
 
 
 
--- chudian-fenbu.py
+++ chudian-fenbu.py
@@ -2,25 +2,28 @@ from ADB.personal.aiplandqudao import set_dglc, xinzeng_aipl, yuanyou_aipl, quda
 from ADB.personal.aiplandqudao import qudaofenbu_huizong
 from ADB import set_pause
 
-set_pause(0.101,0.125)
+set_pause(0.101,0.155)
 set_dglc(2, False, 'dp', 1, 2, 3, 3, 4, '启赋')
 
-ts = '2019-1-1'
-te = '2019-1-31'
-namess = 'V1'
+ts = '2019-2-1'
+te = '2019-2-28'
+namess = 'V2'
 
 #xinzeng_aipl(ts, te, namess)
 #yuanyou_aipl(ts, te, namess)
 
 #input('....')
 
-qudaofenbu_huizong(ts, te, namess)
+
 qudaofenbu_aipl(ts, te, '新增A人群', 1, namess)
 qudaofenbu_aipl(ts, te, '新增I人群', 2, namess)
 qudaofenbu_aipl(ts, te, '新增购买人群', 34, namess)
 
-qudaofenbu_aipl(ts, te, '原有A人群', 1, namess)
+qudaofenbu_huizong(ts, te, namess)
+
 qudaofenbu_aipl(ts, te, '原有I人群', 2, namess)
 qudaofenbu_aipl(ts, te, '原有购买人群', 34, namess)
+qudaofenbu_aipl(ts, te, '原有A人群', 1, namess)
+
 
 
