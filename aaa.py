bcd7273b9b905305f948057a6eccc22871fdc4d1
Ausar109 <Ausar109@aliyun.com>
2019-03-17 21:52:55 +0800

    0317

--- aaa.py
+++ aaa.py
@@ -5,28 +5,28 @@ from datetime import timedelta
 from ADB import shan_renqun
 
 
-set_dglc(2, False, 'dp', 0, 0, 2, 2, 2, '维多利亚的秘密')
+set_dglc(2, False, 'dp', 0, 0, 1, 1, 1, '菁智')
 
-tAs = t180
+tAs = somedays('2019-3-10', 90)
 tIs = t180
-tHs = somedays('2019-3-3', 90)
-tpend = '2019-3-2'
+tHs = somedays('2019-3-10', 90)
+tpend = '2019-3-9'
 
-sousuoci = '维多利亚的秘密,维秘'
+sousuoci = 'Nowords'
 
-fuzhu_of_depth(tAs, tIs, tHs, tpend, sousuoci, pph=False)
+fuzhu_of_depth(tAs, tIs, tHs, tpend, sousuoci)
 input('...')
 depths_of_aipl(tAs, tIs, tpend)
 
 
-t2bef = '2019-3-3'
+t2bef = '2019-3-10'
 t2end = '2019-3-9'
 input('...')
 #diaoyong_jindian(t2bef, t2end)
 #diaoyong_tracking(t2bef, t2end, tpend, 'bg')
 #diaoyong_tracking(t2bef, t2end, tpend, 'scjg')
 #diaoyong_tracking(t2bef, t2end, tpend, 'jg')
-diaoyong_tracking(t2bef, t2end, tpend, 'gm')
+#diaoyong_tracking(t2bef, t2end, tpend, 'gm')
 #diaoyong_tracking(t2bef, t2end, tpend, 'jd')
 
 
